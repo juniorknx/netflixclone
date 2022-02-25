a1f6a01b3d785fb34eaaa857786dfeb465f03bec
Julio Junior <juniorknx@hotmail.com>
2022-02-25 18:07:05 -0300

    Removido botao de salvar

--- src/pages/FilmeDetalhe/index.js
+++ src/pages/FilmeDetalhe/index.js
@@ -14,27 +14,11 @@ export function MovieDetail() {
     useEffect(() => {
         async function GetMovieDetails() {
             const response = await api.get(`/r-api/?api=filmes/${id}`)
-            console.log(response.data);
             setMovie(response.data);
             setLoading(false)
         }
         GetMovieDetails();
     }, [id])
-
-    function salvaFilme() {
-        const minhaLista = localStorage.getItem('filmes');
-        let filmesSalvos = JSON.parse(minhaLista) || [];
-        //Verifica se o filme ja existe
-        const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === movie.id)
-        if(hasFilme){
-            alert('Ja possui esse filme salvo');
-            return;
-        }
-
-        filmesSalvos.push(movie);
-        localStorage.setItem('filmes', JSON.stringify(filmesSalvos))
-        alert('filme salvo!!');
-    }
 
     if (loading === true) {
         return <h1>Carregando..</h1>
@@ -51,7 +35,6 @@ export function MovieDetail() {
                         title={nome}
                         image={foto}
                         description={sinopse}
-                        saveMovie={salvaFilme}
                     />
                 </Wrapper>
             </Container>
